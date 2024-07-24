600a53cc83dd927ac1d2ccae80b73210d64194d8
Dave Banwell <dave@davebanwell.com>
2024-07-24 19:16:54 -0400

    tests all pass

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [isInCart, setIsInCart] = useState(false);
+
+  const cartClass = isInCart ? 'in-cart' : '';
+  const buttonText = isInCart ? 'Remove From Cart' : 'Add to Cart';
+
+  const toggleInCart = () => setIsInCart(!isInCart);
+
   return (
-    <li className="">
+    <li className={cartClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={toggleInCart}>{buttonText}</button>
     </li>
   );
 }
